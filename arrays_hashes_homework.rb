6929e0130b814f4cc487d54a3c03842cb67f3bd3
helenosheaa <helenosheaa@gmail.com>
2018-05-23 17:47:24 +0100

    part 2 question 10 complete

--- arrays_hashes_homework.rb
+++ arrays_hashes_homework.rb
@@ -5,44 +5,44 @@
 stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Haymarket" ]
 
 # Complete these tasks:
-
-#1. Add `"Edinburgh Waverley"` to the end of the array
-stops.push("Edinburgh Waverley")
-# p stops
-
-#2. Add `"Glasgow Queen St"` to the start of the array
-stops.unshift("Glasgow Queen St")
-# p stops
-
-#3. Add `"Polmont"` at the appropriate point (between `"Falkirk High"` and `"Linlithgow"`)
-stops[4] = ("Polmont")
-# p stops
-
-#4. Work out the index position of `"Linlithgow"`
-p stops.index("Linlithgow")
-
-#5. Remove `"Livingston"` from the array using its name
-stops.delete("Livingston")
-# p stops
-
-#6. Delete `"Cumbernauld"` from the array by index
-stops.delete_at(2)
-# p stops
-
-#7. How many stops there are in the array?
-p stops.length
-
-#8. How many ways can we return `"Falkirk High"` from the array?
-p stops[2]
-p stops[-4]
-
-#9. Reverse the positions of the stops in the array
-p stops.reverse
-
-#10. Print out all the stops using a for loop
-for stop in stops
-  p stop
-end
+#
+# #1. Add `"Edinburgh Waverley"` to the end of the array
+# stops.push("Edinburgh Waverley")
+# # p stops
+#
+# #2. Add `"Glasgow Queen St"` to the start of the array
+# stops.unshift("Glasgow Queen St")
+# # p stops
+#
+# #3. Add `"Polmont"` at the appropriate point (between `"Falkirk High"` and `"Linlithgow"`)
+# stops[4] = ("Polmont")
+# # p stops
+#
+# #4. Work out the index position of `"Linlithgow"`
+# p stops.index("Linlithgow")
+#
+# #5. Remove `"Livingston"` from the array using its name
+# stops.delete("Livingston")
+# # p stops
+#
+# #6. Delete `"Cumbernauld"` from the array by index
+# stops.delete_at(2)
+# # p stops
+#
+# #7. How many stops there are in the array?
+# p stops.length
+#
+# #8. How many ways can we return `"Falkirk High"` from the array?
+# p stops[2]
+# p stops[-4]
+#
+# #9. Reverse the positions of the stops in the array
+# p stops.reverse
+#
+# #10. Print out all the stops using a for loop
+# for stop in stops
+#   p stop
+# end
 
 # Exercise B
 
@@ -135,14 +135,15 @@ users = {
 #7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
 # p users["Erik"][:lottery_numbers].push(7)
 
-#8. Change Erik's hometown to Edinburgh
-p users["Erik"][:home_town] = ("Edinburgh")
-
-#9. Add a pet dog to Erik called "Fluffy"
-p users["Erik"][:pets][0].push("Fluffy")
-
-#10. Add another person to the users hash
-
+# #8. Change Erik's hometown to Edinburgh
+# p users["Erik"][:home_town] = ("Edinburgh")
+#
+# #9. Add a pet dog to Erik called "Fluffy"
+# p users["Erik"][:pets]<< {:name => "Fluffy", :species => "Dog"}
+#
+# #10. Add another person to the users hash
+#  users ["Helen"] = []
+#  p users
 
 # Exercise C
 
