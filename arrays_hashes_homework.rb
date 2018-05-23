ec8de9ab2e57c66b3bc2688893ccb1726acb40e0
helenosheaa <helenosheaa@gmail.com>
2018-05-23 21:27:00 +0100

    part 3 question 4 complete

--- arrays_hashes_homework.rb
+++ arrays_hashes_homework.rb
@@ -7,42 +7,42 @@ stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Ha
 # Complete these tasks:
 #
 # #1. Add `"Edinburgh Waverley"` to the end of the array
-# stops.push("Edinburgh Waverley")
-# # p stops
+stops.push("Edinburgh Waverley")
+p stops
 #
 # #2. Add `"Glasgow Queen St"` to the start of the array
-# stops.unshift("Glasgow Queen St")
-# # p stops
+stops.unshift("Glasgow Queen St")
+p stops
 #
 # #3. Add `"Polmont"` at the appropriate point (between `"Falkirk High"` and `"Linlithgow"`)
-# stops[4] = ("Polmont")
-# # p stops
+stops[4] = ("Polmont")
+p stops
 #
 # #4. Work out the index position of `"Linlithgow"`
-# p stops.index("Linlithgow")
+p stops.index("Linlithgow")
 #
 # #5. Remove `"Livingston"` from the array using its name
-# stops.delete("Livingston")
-# # p stops
-#
+stops.delete("Livingston")
+p stops
+
 # #6. Delete `"Cumbernauld"` from the array by index
-# stops.delete_at(2)
-# # p stops
-#
+stops.delete_at(2)
+p stops
+
 # #7. How many stops there are in the array?
-# p stops.length
-#
+p stops.length
+
 # #8. How many ways can we return `"Falkirk High"` from the array?
-# p stops[2]
-# p stops[-4]
-#
+p stops[2]
+p stops[-4]
+
 # #9. Reverse the positions of the stops in the array
-# p stops.reverse
-#
+p stops.reverse
+
 # #10. Print out all the stops using a for loop
-# for stop in stops
-#   p stop
-# end
+for stop in stops
+  p stop
+end
 
 # Exercise B
 
@@ -107,43 +107,43 @@ users = {
 # Complete these tasks:
 
 #1. Get Jonathan's Twitter handle (i.e. the string `"jonnyt"`)
-# p users["Jonathan"][:twitter]
+p users["Jonathan"][:twitter]
 #
 # #2. Get Erik's hometown
-# p users["Erik"][:home_town]
+p users["Erik"][:home_town]
 #
 # #3. Get the array of Erik's lottery numbers
-# p users["Erik"][:lottery_numbers]
+p users["Erik"][:lottery_numbers]
 #
 # #4. Get the type of Avril's pet Monty
-# p users["Avril"][:pets][0][:species]
+p users["Avril"][:pets][0][:species]
 #
 # #5. Get the smallest of Erik's lottery numbers
-# p users["Erik"][:lottery_numbers].min()
+p users["Erik"][:lottery_numbers].min()
 
 #6. Return an array of Avril's lottery numbers that are even
-# lottery_numbers = users["Avril"][:lottery_numbers]
-# even_numbers = []
-# for numbers in lottery_numbers
-#    if numbers % 2 == 0
-#      even_numbers << numbers
-#    end
-#    return even_numbers
-# end
-# p even_numbers
+lottery_numbers = users["Avril"][:lottery_numbers]
+even_numbers = []
+for numbers in lottery_numbers
+   if numbers % 2 == 0
+     even_numbers << numbers
+   end
+   return even_numbers
+end
+p even_numbers
 
 #7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
-# p users["Erik"][:lottery_numbers].push(7)
+p users["Erik"][:lottery_numbers].push(7)
 
 # #8. Change Erik's hometown to Edinburgh
-# p users["Erik"][:home_town] = ("Edinburgh")
+p users["Erik"][:home_town] = ("Edinburgh")
 #
 # #9. Add a pet dog to Erik called "Fluffy"
-# p users["Erik"][:pets]<< {:name => "Fluffy", :species => "Dog"}
+p users["Erik"][:pets]<< {:name => "Fluffy", :species => "Dog"}
 #
 # #10. Add another person to the users hash
-#  users ["Helen"] = []
-#  p users
+ users ["Helen"] = []
+ p users
 
 # Exercise C
 
@@ -169,7 +169,29 @@ united_kingdom = [
 
 # Complete these tasks:
 
-#1. Change the capital of Wales from `"Swansea"` to `"Cardiff"`.
+# #1. Change the capital of Wales from `"Swansea"` to `"Cardiff"`.
+united_kingdom[1][:captial] = ("Cardiff")
+p united_kingdom[1][:captial]
+
 #2. Create a Hash for Northern Ireland and add it to the `united_kingdom` array (The capital is Belfast, and the population is 1,811,000).
+united_kingdom << {
+  name: "Northern Ireland",
+  capital: "Belfast",
+  population: 1811000
+  }
+
+ p united_kingdom
+
 #3. Use a loop to print the names of all the countries in the UK.
+for countries in united_kingdom
+    p countries [:name]
+  end
+
 #4. Use a loop to find the total population of the UK.
+total_population = 0
+for uk_population in united_kingdom
+  total_population +=
+  uk_population[:population]
+  uk_population[:population] = 0
+end
+p total_population
